bf11e4aad26c64b71adc5f938462303419b0beb3
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-11-12 23:50:54 +0300

    Init project, add stylelint and eslint

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+
+import {AppProps} from "next/app";
+import {JSX} from "react";
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
