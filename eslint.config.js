79b506a2bfad6a47c96dea9ef2fe92f169e4d968
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-11-13 23:27:31 +0300

    Add eslint configs

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import nextPlugin from '@next/eslint-plugin-next';
+import globals from 'globals';
+import reactPlugin from 'eslint-plugin-react';
+
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules', '.next', 'dist', 'eslint.config.js'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      '@next/next': nextPlugin,
+      react: reactPlugin,
+    },
+    rules: {
+      // базовые рекомендации
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+
+      // индивидуальные настройки
+      semi: ['warn', 'always'],
+      '@typescript-eslint/no-empty-interface': [
+        'error',
+        {allowSingleExtends: true},
+      ],
+      'no-multiple-empty-lines': ["error", {
+        "max": 1,
+        "maxEOF": 0,
+        "maxBOF": 0
+      }],
+      'react/jsx-indent': ["error", 2],
+      'react/jsx-indent-props': ["error", 2],
+      'react/jsx-curly-spacing': ["error", {
+        "when": "always",
+        "children": true
+      }],
+      'object-curly-spacing': ["error", "always"]
+    },
+    settings: {
+      react: {
+        version: 'detect', // автоматически определяет версию React
+      },
+    },
+  },
+];
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
-
 import {AppProps} from "next/app";
 import {JSX} from "react";
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
