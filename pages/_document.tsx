30a6abc55cd644340d2f751642292249052691a5
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-11-13 23:29:17 +0300

    Override document structure with lang ru in html tag

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from "next/document";
+import { JSX } from "react";
+
+class MyDocument extends Document {
+  static async getInitialProps(context: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(context);
+
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang='ru'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
