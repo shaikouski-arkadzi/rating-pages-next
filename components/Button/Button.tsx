227acd04ef437372d2e850ac18c67a2ccada7091
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-11-14 22:43:27 +0300

    Add HtmlButton default props to Button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,17 +1,23 @@
 import { JSX } from "react";
-import cn from 'classnames';
-import { ButtonProps } from './Button.props';
-import styles from './Button.module.css';
+import cn from "classnames";
+import { ButtonProps } from "./Button.props";
+import styles from "./Button.module.css";
 
-export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
+export const Button = ({
+  appearance,
+  children,
+  className,
+  ...props
+}: ButtonProps): JSX.Element => {
   return (
     <button
-      className={ cn(styles.button, {
-        [styles.primary]: appearance == 'primary',
-        [styles.ghost]: appearance == 'ghost',
-      }) }
+      className={cn(styles.button, className, {
+        [styles.primary]: appearance == "primary",
+        [styles.ghost]: appearance == "ghost",
+      })}
+      {...props}
     >
-      { children }
+      {children}
     </button>
   );
 };
