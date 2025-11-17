dafc3d8cd332eaee0082102e328aa2d2857c2355
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-11-17 08:30:54 +0300

    Add Layout component styles

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,5 +2,5 @@ import { JSX } from "react";
 import { SidebarProps } from "./Sidebar.props";
 
 export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+  return <div {...props}>Sidebar</div>;
 };
