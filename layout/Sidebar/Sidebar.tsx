d669d82c04b8e32f195bd3815f26926384f379fd
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-11-25 09:06:22 +0300

    Add sidebar logo and styles

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,15 @@
 import { JSX } from "react";
+import cn from "classnames";
 import { SidebarProps } from "./Sidebar.props";
 import { Menu } from "../Menu/Menu";
+import Logo from "../../icons/logo.svg";
+import styles from "./Sidebar.module.css";
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
-    <div {...props}>
+    <div className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div>поиск</div>
       <Menu />
     </div>
   );
