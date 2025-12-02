e16dfac8e4ea3d066e455d1ab0e2a595a41acfcb
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-12-02 09:18:24 +0300

    Add word ending function

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -3,7 +3,7 @@ import { Card } from "../Card/Card";
 import { Rating } from "../Rating/Rating";
 import { Tag } from "../Tag/Tag";
 import { Button } from "../Button/Button";
-import { priceRu } from "../../helpers/helpers";
+import { declOfNum, priceRu } from "../../helpers/helpers";
 import { Divider } from "../Divider/Divider";
 import { ProductProps } from "./Product.props";
 import styles from "./Product.module.css";
@@ -38,7 +38,10 @@ export const Product = ({ product }: ProductProps): JSX.Element => {
       </div>
       <div className={styles.priceTitle}>цена</div>
       <div className={styles.creditTitle}>кредит</div>
-      <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+      <div className={styles.rateTitle}>
+        {product.reviewCount}
+        {declOfNum(product.reviewCount, ["отзыв", "отзыва", "отзывов"])}
+      </div>
       <Divider className={styles.hr} />
       <div className={styles.description}>{product.description}</div>
       <div className={styles.feature}>фичи</div>
